6e3c0227c0649cf996c134d969cf804c8d24e7b2
Samantha brake <sammy.brake@gmail.com>
2018-11-13 01:33:08 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -27,9 +27,21 @@ def initialize(options_hash={})
   end
 end
 
+self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
+
 def table_name_for_insert
 self.class.table_name
 end
+
+def initialize(options_hash={})
+  options_hash.each do |key, value|
+    self.send("#{key}=", value)
+  end
+end
+
 
 def col_names_for_insert
   column_names = []
@@ -45,6 +57,22 @@ def values_for_insert
     values << "'#{send(col_name)}'" unless send(col_name).nil?
   end
   values.join(", ")
+  end
+
+def save
+  sql = <<-SQL
+  INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})
+  SQL
+
+  DB[:conn].execute(sql)
+
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+end
+
+def self.find_by_name(name)
+  sql = "SELECT * FROM #{table_name} WHERE name = ?"
+
+  DB[:conn].execute(sql, name )
 end
 
 end
