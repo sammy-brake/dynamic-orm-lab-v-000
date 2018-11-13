6b4f6b60bc9df5939136cf45ab3ac518708bf077
Samantha brake <sammy.brake@gmail.com>
2018-11-13 02:33:08 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -82,8 +82,8 @@ def self.find_by(attribute)
   attribute.each do |key, value|
     sql << "SELECT * FROM #{table_name} WHERE ? = ?"
     col << "#{key}"
-    value.is_a? Integer ? val << "#{value}" : val << "'#{value}'"
-    
+    val << value.is_a? Integer ? "#{value}" : "'#{value}'"
+
   end
   DB[:conn].execute(sql, col, val)
 binding.pry
